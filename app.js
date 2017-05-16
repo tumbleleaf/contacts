64f2db87760d04f22d57f72756e1a13714bf6751
Ray Ruiz <rruiz@thexyz.org>
2017-05-16 17:16:46 -0400

    added mongo crud operations

--- app.js
+++ app.js
@@ -1,105 +1,70 @@
-var express = require('express');
-var path = require('path');
-
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var contacts = require('./modules/contacts');
-var http = require('http');
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+  , bodyParser = require('body-parser')
+  , logger = require('morgan')
+  , methodOverride = require('method-override')
+  , errorHandler = require('errorhandler')
+  , mongoose = require('mongoose')
+  , dataservice = require('./modules/contactdataservice');
+var app = express();
 var url = require('url');
 
-var app = express();
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine','jade');
-
-app.use(logger('dev'));
+app.use(methodOverride());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
 
-//app.use('/',routes);
-//app.use('/users',users);
-
-// catch 404 and forward to error handler
-/*
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env')==='development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+// development only
+if ('development' == app.get('env')) {
+  app.use(errorHandler());
 }
 
-// production error handler
-// will print stacktrace
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-*/
-app.get('/contacts',
-        function(request, response){
-            var get_params = url.parse(request.url,
-            true).query;
+mongoose.connect('mongodb://localhost/contacts');
 
-            if (Object.keys(get_params).length === 0)
-            {
-                response.setHeader('content-type',
-                'application/json');
-                response.end(JSON.stringify
-                (contacts.list()));
-            }
-            else
-            {
-                response.setHeader('content-type',
-                'application/json');
-                response.end(JSON.stringify
-                (contacts.query_by_arg(get_params.arg,
-                get_params.value)));
-            }
-        }
-);
+var contactSchema = new mongoose.Schema({
+  primarycontactnumber: {type: String, index: {unique:true}},
+  firstname: String,
+  lastname: String,
+  title: String,
+  company: String,
+  jobtitle: String,
+  othercontactnumbers: [String],
+  primaryemailaddress: String,
+  emailaddresses: [String],
+  groups: [String]
+});
+var Contact = mongoose.model('Contact', contactSchema);
 
 app.get('/contacts/:number', function(request, response) {
-    response.setHeader('content-type','application/json');
-    response.end(JSON.stringify
-    (contacts.query(request.params.number)));
+  console.log(request.url + ' : querying for ' +
+  request.params.number);
+  dataservice.findByNumber(Contact, request.params.number,
+  response);
 });
 
-app.get('/groups', function(request, response) {
-    console.log ('groups');
-    response.setHeader('content-type',
-    'application/json');
-    response.end(JSON.stringify(contacts.list_groups()));
+app.post('/contacts', function(request, response) {
+  dataservice.update(Contact, request.body, response);
 });
 
-app.get('/groups/:name', function(request, response) {
-    console.log ('groups');
-    response.setHeader('content-type',
-    'application/json');
-    response.end(JSON.stringify
-    (contacts.get_members(request.params.name)));
+app.put('/contacts', function(request, response) {
+  dataservice.create(Contact, request.body, response);
 });
-/*
-http.createServer(app).listen(3000, function(){
-  console.log('Express server listening on port 3000');
+
+app.delete('/contacts/:primarycontactnumber', function(request, response) {
+  dataservice.remove(Contact, request.params.primarycontactnumber, response);
 });
-*/
+
+app.get('/contacts', function(request, response) {
+  console.log('Listing all contacts with ' +
+  request.params.key + '=' + request.params.value);
+  dataservice.list(Contact, response);
+});
+
+console.log('Running at port ' + app.get('port'));
+//http.createServer(app).listen(app.get('port'));
 
 module.exports = app;
